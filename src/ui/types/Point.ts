fe10a6a4b069557ae79ecfae475cc8e93a900a13
odin3 <ascii@live.ru>
2017-10-07 23:27:11 +0300

    Add docs to Point

--- src/ui/types/Point.ts
+++ src/ui/types/Point.ts
@@ -2,6 +2,11 @@ export class Point {
   public x: number;
   public y: number;
 
+  /**
+   * Create a new point
+   * @param {number} x
+   * @param {number} y
+   */
   public constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
